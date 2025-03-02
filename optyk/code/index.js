6665939fc90393338c004c56631b2cc5f3acca29
Ania <annaradlinska99@gmail.com>
2025-03-02 11:57:07 +0100

    final version

---
+++ optyk/code/index.js
@@ -0,0 +1,46 @@
+// Karuzela
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+initializeSlider();
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    setInterval(nextSlide, 6000);
+  }
+}
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
+// Galeria produktu
+
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function () {
+  ProductImg.src = SmallImg[0].src;
+};
+SmallImg[1].onclick = function () {
+  ProductImg.src = SmallImg[1].src;
+};
+SmallImg[2].onclick = function () {
+  ProductImg.src = SmallImg[2].src;
+};
